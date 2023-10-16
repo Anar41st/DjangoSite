a5ee2132d29a50a307ec4a9f7d11e2956488c9ee
Anar41st <nrbchat@mail.ru>
2023-10-16 21:12:49 +0300

    переделал 500 ошибку ещё раз

--- LastTry/main/urls.py
+++ LastTry/main/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 register_converter(FourDigitYearConverter, "yyyy")
 urlpatterns = [
-    path('', views.index),
-    path('Student/<int:studentID>/', views.StudentList),
-    path('YEAR/', views.years_mainpage),
-    path('years/<yyyy:year_id>/', views.year),
+    path('', views.index, name='homepage'),
+    path('Student/<int:studentID>/', views.StudentList, name='Students'),
+    path('year/', views.years_mainpage, name='yearsHomepage'),
+    path('year/<yyyy:year_id>/', views.year, name='year'),
     path('err400/', views.err400),
     path('err500/', views.err500),
 ]
--- LastTry/main/views.py
+++ LastTry/main/views.py
@@ -1,27 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseForbidden, HttpResponseServerError, HttpResponseBadRequest
 from django.core.exceptions import PermissionDenied, BadRequest
 
 def index(request):
-    return HttpResponse("<h4>Проверка работы</h4>")
+    data = {
+        'title': 'Главная страница',
+          }
+    return render(request, 'main/index.html')
 def StudentList(request,studentID):
-    StudentsDict = {1: '1) Буренок Дмитрий 14.01.2005',
-                    2: '2) Горбанёв Кирилл 25.01.2006',
-                    3: '3) Капшукова Дарья 27.06.2004',
-                    4: '4) Кашаева Раяна 27.06.2004',
-                    5: '5) Климин Тимур 17.05.2004',
-                    6: '6) Косенков Глеб 9.06.2004',
-                    7: '7) Костин Максим 3.04.2001',
-                    8: '8) Кузенков Богдан Н/Д',
-                    9: '9) Миколадзе Антон 14.09.2004',
-                    10: '10) Мишин Александр 21.08.2004',
-                    11: '11) Мишин Алексей 21.08.2004',
-                    12: '12) Пешеходько Арсений Н/Д',
-                    13: '13) Сентюрина Екатерина 08.11.2002'}
+    StudentsDict = {1: '1) Буренок Дмитрий ',
+                    2: '2) Горбанёв Кирилл ',
+                    3: '3) Капшукова Дарья ',
+                    4: '4) Кашаева Раяна ',
+                    5: '5) Климин Тимур ',
+                    6: '6) Косенков Глеб ',
+                    7: '7) Костин Максим ',
+                    8: '8) Кузенков Богдан ',
+                    9: '9) Миколадзе Антон ',
+                    10: '10) Мишин Александр ',
+                    11: '11) Мишин Алексей ',
+                    12: '12) Пешеходько Арсений ',
+                    13: '13) Сентюрина Екатерина '}
     if 1 <= studentID <= 13:
         return HttpResponse(f"<h2>Студент</h2> <p>{StudentsDict[studentID]}</p>")
     else:
-        return HttpResponse("<h1>Студент не найден</h1>")
+        return redirect('student', '2')
 
 def year(reguest, year_id):
     events = {2000: '<li> 2000</li>',
@@ -52,7 +55,8 @@ def year(reguest, year_id):
     if 2000 <= int(year_id) <= 2023:
         return HttpResponse(f"<h1>Основные события {year_id} года</h1> <ul>{events[int(year_id)]}</ul>")
     else:
-        return HttpResponse(f"<h1>{year_id} год отсутствует в списке</h1>")
+        return redirect('yearsHomepage')
+        #return HttpResponse(f"<h1>{year_id} год отсутствует в списке</h1>")
 
 def years_mainpage(request):
     if(request.GET):
