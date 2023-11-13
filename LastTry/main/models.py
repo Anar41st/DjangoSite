73faaf281c081820e96a4cf90ff07ac7582ac770
Anar41st <nrbchat@mail.ru>
2023-11-13 21:55:07 +0300

    НЕ удаляется таблица

--- LastTry/main/models.py
+++ LastTry/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+data_db=[
+    {'id':1,'name':'Борисыч','interes':'risovanie , football','price':'2000'},
+    {'id':2,'name':'ktru','genre':'grid , fooll','price':'1000'},
+    {'id':3,'name':'ktru','genre':'grid , fooll','price':'5'},
+    {'id':4,'name':'ktru','genre':'grid , fooll','price':'100'},
+    {'id':5,'name':'ktru','genre':'grid , fooll','price':'10'},
+    {'id':6,'name':'ktru','genre':'grid , fooll','price':'1'},
+    {'id':7,'name':'ktru','genre':'grid , fooll','price':'1000000'}
+]
 
+class Books(models.Model):
+    name=models.TextField(blank=True)
+    genre=models.TextField(blank=True)
+    time_create= models.DateTimeField(auto_now_add=True)
+    time_update=models.DateTimeField(auto_now=True)
+    price=models.IntegerField(blank=True)
